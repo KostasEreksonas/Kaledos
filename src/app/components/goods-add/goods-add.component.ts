c75db5c320923f6c85698593d51584da29fcdd5b
KostasEreksonas <k.ereksonas@gmail.com>
2024-10-26 22:48:33 +0300

    Sutaisytas kelias iki bootstrap bibliotekos

--- src/app/components/goods-add/goods-add.component.ts
+++ src/app/components/goods-add/goods-add.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import {GoodsService} from '../../services/goods.service';
 import {Good} from '../../models/good';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-goods-add',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './goods-add.component.html',
   styleUrl: './goods-add.component.css'
 })
 export class GoodsAddComponent {
+  public recipient:string|null = null;
+  public description:string|null = null;
+  public status:string|null = null;
+
   public constructor(private goodsService:GoodsService) {
 
   }
